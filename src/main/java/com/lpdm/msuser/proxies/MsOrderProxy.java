33a603a4dfbe176fd817c1576e4f70bacb7e2814
julien <juliencauwet@yahoo.fr>
2018-12-11 16:13:22 +0100

    added method to MsOrderProxy

--- src/main/java/com/lpdm/msuser/proxies/MsOrderProxy.java
+++ src/main/java/com/lpdm/msuser/proxies/MsOrderProxy.java
@@ -1,11 +1,17 @@
 package com.lpdm.msuser.proxies;
 
 import com.lpdm.msuser.msorder.OrderBean;
+import com.lpdm.msuser.msorder.PaymentBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Component
 @FeignClient(name = "ms-order", url = "localhost:28083")
@@ -13,4 +19,14 @@ public interface MsOrderProxy {
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     OrderBean getOrderById(@PathVariable int id);
+
+    @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    OrderBean saveOrder(@Valid @RequestBody OrderBean order);
+
+    @GetMapping(value = "/by/customer/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    List<OrderBean> findAllByUserId(@PathVariable int id);
+
+    @GetMapping(value = "/payments", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    List<PaymentBean> getPaymentList();
+
 }
