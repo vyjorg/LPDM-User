27aee36bf84121df827a3bee8f24150774f1de94
vianney diris <vianney.diris@gmail.com>
2019-02-12 18:47:10 +0100

    Ajout vérification pour price and tax

--- src/main/resources/static/js/producer/addProduct.js
+++ src/main/resources/static/js/producer/addProduct.js
@@ -12,4 +12,16 @@ $(document).ready(function() {
     $('#status').change(function(){
         $('#check_status').attr("class", "btn btn-success");
     })
+
+    // Check inputs price and tax
+    $(":input").on("input change when keyup", function () {
+        let input = $(this).attr("id");
+        if(input.match("price") || input.match("tax")){
+            this.value=this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+        }
+
+        let button = $(this).next("div").find("button");
+        if($(this).val() !== "") button.attr("class", "btn btn-success");
+        else button.attr("class", "btn btn-danger");
+    });
 })
